d7b256f4b769b4e895ca4e3bcb467a881c44e97d
Leo777 <borichevskiy@gmail.com>
2019-01-23 11:12:23 +0100

    Integration test

--- src/test/java/com/skelia/mailSender/mailSenderService/integration/GraphSendServiceIntegrationTest.java
+++ src/test/java/com/skelia/mailSender/mailSenderService/integration/GraphSendServiceIntegrationTest.java
@@ -1,11 +1,16 @@
 package com.skelia.mailSender.mailSenderService.integration;
 
+import com.skelia.mailSender.mailSenderService.services.GraphSendService;
 import org.apache.commons.io.FileUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import java.io.File;
 import java.io.FileReader;
@@ -13,24 +18,33 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Base64;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GraphSendServiceIntegrationTest {
 
-    //To Do Integration test
+    @Autowired
+    private GraphSendService sendService;
 
+    @Test
+    public void testGraphMailSender(){
+       JSONObject message = readMessageFromFile();
+      ResponseEntity responseEntity = sendService.send(message,"leu.barycheuski@skelia.com");
+       assertEquals(HttpStatus.ACCEPTED,responseEntity.getStatusCode());
+    }
 
-    public JSONObject readingJsonFromFile() {
+    private JSONObject readMessageFromFile() {
 
         JSONParser jsonParser = new JSONParser();
 
-        File file = new File("../main/resources/mail.json");
+        File file = new File("./src/main/resources/mail.json");
 
         Object object = null;
         byte[] fileContent = null;
         try {
             object = jsonParser.parse(new FileReader(file));
-            fileContent = FileUtils.readFileToByteArray(new File("/main/resources/hello world.txt"));
+            fileContent = FileUtils.readFileToByteArray(new File("./src/main/resources/hello world.txt"));
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ParseException e) {
